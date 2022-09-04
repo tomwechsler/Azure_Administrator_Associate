045ac0d974236935453be7f40b7ee5fc2a74ccd9
Tom Wechsler <tom.wechsler@outlook.com>
2022-09-04 07:03:07 +0200

    new file

---
+++ install-iis..ps1
@@ -0,0 +1,4 @@
+﻿# Installs the web-server feature and creates a static page displaying the VM hostname
+
+Install-WindowsFeature -Name 'web-server'
+Add-Content -Path "c:\inetpub\wwwroot\default.htm" -Value $($env:computername)
